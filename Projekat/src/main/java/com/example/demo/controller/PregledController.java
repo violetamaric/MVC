ac1c9f6e3d40adc49d3c079b2b217c8c5c4b0a14
violetamaric <violetam555@gmail.com>
2020-01-04 22:12:02 +0100

    promene prilikom kreiranja zahteva za pregled

--- Projekat/src/main/java/com/example/demo/controller/PregledController.java
+++ Projekat/src/main/java/com/example/demo/controller/PregledController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -53,6 +54,7 @@ public class PregledController {
 
 	@PostMapping(path="/new", consumes = "application/json")
 	@CrossOrigin(origins = "http://localhost:3000")
+	@PreAuthorize("hasAuthority('PACIJENT')")
 	public ResponseEntity<PregledDTO> noviPregled(@RequestBody PregledDTO pregledDTO) {
 		System.out.println("dodavanje novog pregleda");
 		System.out.println(pregledDTO);
